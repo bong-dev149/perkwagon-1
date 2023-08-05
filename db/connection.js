451e8bc88d84e225c14b12291dd3cffa980b631e
Sayan <sayanbhowal123@gmail.com>
2023-08-05 15:29:00 +0530

    "new database"

--- db/connection.js
+++ db/connection.js
@@ -7,6 +7,9 @@ const config = require('/etc/secrets/config.json')[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
+    port: config.port,
 });
+
+//const sequelize = new Sequelize('mysql://doadmin:AVNS_MNHtg4fuAOBWyfP4OAS@perkwagon-db-do-user-14449570-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED')
 
 module.exports = sequelize;  
