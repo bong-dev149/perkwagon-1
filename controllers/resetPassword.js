c96880337cee546fa3101119f1cb15cb55f264eb
Sayan <sayanbhowal123@gmail.com>
2023-08-17 00:07:05 +0530

    change cnfmail

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -1,16 +1,16 @@
 
 const resetPassword = async (req, res) => {
     try {
-        if(!req.error){
+        if (!req.error) {
             //get data from request query
             const resetToken = req.query.token;
 
             //send response
             res.status(200).json({ msg: "New password form", token: resetToken })
         }
-        else{
+        else {
             //send response
-            res.status(400).json({error:req.error});
+            res.status(400).json(req.error);
         }
     } catch (err) {
         res.status(400).json(err.message)
